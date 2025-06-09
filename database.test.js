a97958f76243ec45a248656da836dcc58c8d2a0c
Marco Canaza <mortzkeb@gmail.com>
2025-06-09 16:01:48 +0000

    Se agregan nuevos tests para los nuevas columnas solicitadas en el #2

--- database.test.js
+++ database.test.js
@@ -136,10 +136,111 @@ describe('Test database', () => {
 
     test('Insert a user without city', async () => {
       const query = `INSERT INTO
-                     users (email, username, birthdate)
-                     VALUES ('user@example.com', 'user', '2024-01-02')`
+                     users (email, username, birthdate, first_name, last_name, password, enabled)
+                     VALUES ('user@example.com', 'user', '2024-01-02', 'Juan', 'Perez', 'hashed_pass_123', true)`
 
       await expect(client.query(query)).rejects.toThrow('null value in column "city"')
+    })
+    test('Insert a user without the first name', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city,  last_name, password, enabled)
+                     VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata', 'Perez', 'hashed_pass_123', true)`
+
+      await expect(client.query(query)).rejects.toThrow('null value in column "first_name"')
+    })
+    test('Insert a user without the last name', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city,  first_name, password, enabled)
+                     VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata', 'Juan', 'hashed_pass_123', true)`
+
+      await expect(client.query(query)).rejects.toThrow('null value in column "last_name"')
+    })
+    test('Insert a user without the password', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city, first_name, last_name, enabled)
+                     VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata', 'Juan', 'Perez', true)`
+
+      await expect(client.query(query)).rejects.toThrow('null value in column "password"')
+    })
+    test('Insert a user with an invalid value in the enabled field', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city, first_name, last_name, password, enabled)
+                     VALUES ('user@example.com', 'user', 'invalid_date', 'La Plata', 'Juan', 'Perez', 'hashed_pass_123', 1952)`
+
+      await expect(client.query(query)).rejects.toThrow('invalid input syntax for type date')
+    })
+    test('Insert a user with an invalid value in the created_at field', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city, first_name, last_name, password, enabled, created_at)
+                     VALUES ('user@example.com', 'user', 'invalid_date', 'La Plata', 'Juan', 'Perez', 'hashed_pass_123', true, 1982)`
+
+      await expect(client.query(query)).rejects.toThrow('invalid input syntax for type date')
+    })
+    test('Insert a user with an invalid value in the updated_at field', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city, first_name, last_name, password, enabled, updated_at)
+                     VALUES ('user@example.com', 'user', 'invalid_date', 'La Plata', 'Juan', 'Perez', 'hashed_pass_123', true, 1982)`
+
+      await expect(client.query(query)).rejects.toThrow('invalid input syntax for type date')
+    })
+    test('Insert a user with an invalid value in the last_access_time field', async () => {
+      const query = `INSERT INTO
+                     users (email, username, birthdate, city, first_name, last_name, password, enabled, last_access_time)
+                     VALUES ('user@example.com', 'user', 'invalid_date', 'La Plata', 'Juan', 'Perez', 'hashed_pass_123', true, 1982)`
+
+      await expect(client.query(query)).rejects.toThrow('invalid input syntax for type date')
+    })
+  })
+  describe('Validate insertion', () => {
+    afterEach(async () => {
+      await client.query('TRUNCATE users')
+    })
+    test('Updating the data of a valid user', async () => {
+      let result = await client.query(
+        `INSERT INTO
+         users (email, username, birthdate, city, first_name, last_name, password, enabled)
+         VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata', 'Juan', 'Perez', 'hashed_pass_123', true)`
+      )
+
+      expect(result.rowCount).toBe(1)
+      const update = await client.query(
+        `UPDATE users
+         SET city = 'Buenos Aires', password = 'new_hashed_pass'
+         WHERE username = 'user'`
+      )
+
+      expect(update.rowCount).toBe(1)
+
+      result = await client.query(
+        'SELECT * FROM users'
+      )
+
+      const user = result.rows[0]
+
+      expect(user.city).toBe('Buenos Aires')
+      expect(user.password).toBe('new_hashed_pass')
+    })
+    test('Updating the data and verifying the change in the updated_at field', async () => {
+      let result = await client.query(
+        `INSERT INTO
+         users (email, username, birthdate, city, first_name, last_name, password, enabled)
+         VALUES ('user@example.com', 'user', '2024-01-02', 'La Plata', 'Juan', 'Perez', 'hashed_pass_123', true)`
+      )
+      expect(result.rowCount).toBe(1)
+
+      const update = await client.query(
+        `UPDATE users
+         SET updated_at = NOW()
+         WHERE username = 'user'`
+      )
+      expect(update.rowCount).toBe(1)
+
+      result = await client.query(
+        'SELECT * FROM users'
+      )
+
+      const user = result.rows[0]
+      expect(user.updated_at).not.toBe(user.created_at)
     })
   })
 })
